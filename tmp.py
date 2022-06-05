77115bd801249ad9a33498664aa89ac2eb71ea6f
Razok <yur4ik77@gmail.com>
2022-06-05 16:02:40 +0300

    fix: fixed data

--- tmp.py
+++ tmp.py
@@ -20,33 +20,49 @@ with open("data2.csv", "r") as stream:
         break
 
 
+idd = 0
+
 
 for e in data1:
     el = {
         "category": e.get("category"),
-        "name": e.get("name"),
+        "name": data2[idd][2].replace(r"%2C", ","),
         "discount": int(e.get("discount")),
-        "price": float(e.get("price"), ),
+        "price": float(data2[idd][3]),
         "count": 0,
-        "image": e.get("image"),
+        "image": data2[idd][2],
         "id": int(e.get("id"))
     }
+    idd += 1
     result.append(el)
 
 
-id = 101
-for e in data2:
-    el = {
-        "category": e[0],
-        "name": e[2].replace(r"%2C", ","),
-        "discount": int(random.random() * 30),
-        "price": float(e[3]),
-        "count": 0,
-        "image": e[1],
-        "id": id
-    }
-    id += 1
-    result.append(el)
+# for e in data1:
+#     el = {
+#         "category": e.get("category"),
+#         "name": e.get("name"),
+#         "discount": int(e.get("discount")),
+#         "price": float(e.get("price")),
+#         "count": 0,
+#         "image": e.get("image"),
+#         "id": int(e.get("id"))
+#     }
+#     result.append(el)
+
+
+# id = 101
+# for e in data2:
+#     el = {
+#         "category": e[0],
+#         "name": e[2].replace(r"%2C", ","),
+#         "discount": int(random.random() * 30),
+#         "price": float(e[3]),
+#         "count": 0,
+#         "image": e[1],
+#         "id": id
+#     }
+#     id += 1
+#     result.append(el)
 
 with open("result.json", "w") as stream:
     stream.write(json.dumps(result).encode("utf-8").decode("utf-8"))
